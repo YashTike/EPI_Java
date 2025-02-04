e10d3ed7f00fd9a20df5a6ebab15e2eb6efd80a0
YashTike <yashtike.usa@gmail.com>
2025-02-04 06:42:38 -0800

    Completed CountBits - Chapter 4 Bootcamp

--- epi_judge_java/epi/CountBits.java
+++ epi_judge_java/epi/CountBits.java
@@ -4,9 +4,19 @@ import epi.test_framework.GenericTest;
 public class CountBits {
   @EpiTest(testDataFile = "count_bits.tsv")
 
+  //Program to count number of bits set to 1 in a non-negative integer
   public static short countBits(int x) {
-    // TODO - you fill in here.
-    return 0;
+
+    short numBits = 0;
+
+    while (x != 0) {
+      //Using AND operation with 1 to count LSB in each pass,
+      //and then shifting right to move on to next bit
+      numBits += x & 1;
+      x >>>= 1;
+    }
+
+    return numBits;
   }
 
   public static void main(String[] args) {
